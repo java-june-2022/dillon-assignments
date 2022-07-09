83fba0ae55ba53dc2ac0da99a2478995ca8a512f
tooba GG <dsereduk@gmail.com>
2022-07-08 18:47:36 -0700

    initial

---
+++ Week1/Cafe Business Logic/CafeJava.java
@@ -0,0 +1,54 @@
+public class CafeJava {
+    public static void main(String[] args) {
+        // APP VARIABLES
+        // Lines of text that will appear in the app. 
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
+        
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        int dripCoffeePrice = 5;
+        int lattePrice = 2;
+        double cappucinoPrice = 4.99;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+    	// ** Your customer interaction print statements will go here ** //
+        //Cindhuri Interaction//
+        System.out.println(customer1 + pendingMessage);
+        //Noah Interaction//
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + cappucinoPrice);
+        }
+        else {
+            System.out.println(customer4 + pendingMessage);
+        }
+        //Sam Interaction//
+        System.out.println(displayTotalMessage + lattePrice * 2);
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        }
+        else {
+            System.out.println(customer2 + pendingMessage);
+        }
+        //Jimmy Interaction//
+        System.out.println(displayTotalMessage + (lattePrice - dripCoffeePrice));
+
+    }
+
+}
---
+++ Week1/Extra Practice/First_Time.java
@@ -0,0 +1,7 @@
+public class First_Time {
+    public static void main(String[] args) {
+        System.out.println("My name is Dillon Sereduk");
+        System.out.println("My homestate is Arizona");
+        System.out.println("I am 20 years old");
+    }
+}
