e5c54cd03e15cd52eb938b481e208a9b87ff68d8
tooba GG <dsereduk@gmail.com>
2022-07-19 10:48:55 -0700

    init

---
+++ Week2/Zoo Keeper Part 2/Mammal.java
@@ -0,0 +1,11 @@
+public class Mammal {
+    int energyLevel = 100;
+
+    int displayEnergy(){
+        System.out.printf("Energy level: %s\n", energyLevel);
+        return energyLevel;
+    }
+    public Mammal(int energyLevel){
+        this.energyLevel = energyLevel;
+    }
+}
